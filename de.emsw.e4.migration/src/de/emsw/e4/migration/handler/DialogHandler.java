f28dc8ccb493844fc55adfc305872463ef77222a
Christoph Keimel <c.keimel@emsw.de>
2015-03-26 17:01:19 +0100

    [javafx] Migrate @Named(SWTServiceConstants.ACTIVE_SHELL) injections

--- de.emsw.e4.migration/src/de/emsw/e4/migration/handler/DialogHandler.java
+++ de.emsw.e4.migration/src/de/emsw/e4/migration/handler/DialogHandler.java
@@ -4,7 +4,7 @@ package de.emsw.e4.migration.handler;
 import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.fx.runtime.swt.e4.SWTServiceConstants;
 import org.eclipse.swt.widgets.Shell;
 
 import de.emsw.e4.migration.dialog.SampleTitleAreaDialog;
@@ -12,7 +12,7 @@ import de.emsw.e4.migration.dialog.SampleTitleAreaDialog;
 public class DialogHandler {
 	
 	@Execute
-	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell shell) {
+	public void execute(@Named(SWTServiceConstants.ACTIVE_SHELL) Shell shell) {
 		SampleTitleAreaDialog dialog = new SampleTitleAreaDialog(shell);
 		dialog.open();
 	}
--- de.emsw.e4.migration/src/de/emsw/e4/migration/handler/MessageHandler.java
+++ de.emsw.e4.migration/src/de/emsw/e4/migration/handler/MessageHandler.java
@@ -4,15 +4,13 @@ package de.emsw.e4.migration.handler;
 import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.fx.runtime.swt.e4.SWTServiceConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 
 public class MessageHandler {
-	
 	@Execute
-	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell shell) {
+	public void execute(@Named(SWTServiceConstants.ACTIVE_SHELL) Shell shell) {
 		MessageDialog.openInformation(shell, "Message", "Hello World!");
 	}
-		
 }
