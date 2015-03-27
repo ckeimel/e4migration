68d494bad20f4b3535da962830381058d260218d
Christoph Keimel <c.keimel@emsw.de>
2015-03-27 15:14:12 +0100

    [javafx] added custom BundleTranslationProvider to handle menu mnemonics

--- de.emsw.e4.migration/src/de/emsw/e4/migration/AppLifeCycle.java
+++ de.emsw.e4.migration/src/de/emsw/e4/migration/AppLifeCycle.java
@@ -2,7 +2,11 @@ package de.emsw.e4.migration;
 
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.internal.services.BundleTranslationProvider;
+import org.eclipse.e4.core.services.translation.TranslationService;
+import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
+import org.eclipse.e4.ui.workbench.lifecycle.ProcessAdditions;
 
 public class AppLifeCycle {
 	
@@ -13,6 +17,18 @@ public class AppLifeCycle {
 		if (!result) {
 			System.exit(0);
 		}
+	}
+
+	@ProcessAdditions
+	public void processAdditions(MApplication application) {
+		TranslationService translationProvider = new BundleTranslationProvider() {
+			@Override
+			public String translate(String key, String contributorURI) {
+				String result = super.translate(key, contributorURI);
+				return result.replace('&', '_');
+			}
+		};
+		application.getContext().set(TranslationService.class, translationProvider);
 	}
 	
 }
