d6e9ad7abde7a0666979261dda31b2d00919bce2
Christoph Keimel <c.keimel@emsw.de>
2015-04-01 12:31:29 +0200

    [javafx] Aborting the startup

--- de.emsw.e4.migration/src/de/emsw/e4/migration/AppLifeCycle.java
+++ de.emsw.e4.migration/src/de/emsw/e4/migration/AppLifeCycle.java
@@ -7,16 +7,18 @@ import org.eclipse.e4.core.services.translation.TranslationService;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
 import org.eclipse.e4.ui.workbench.lifecycle.ProcessAdditions;
+import org.eclipse.fx.ui.services.restart.LifecycleRV;
 
 public class AppLifeCycle {
 	
 	@PostContextCreate
-	public void postContextCreate(IEclipseContext context) {
+	public LifecycleRV postContextCreate(IEclipseContext context) {
 		LoginService loginService = ContextInjectionFactory.make(LoginService.class, context);
 		boolean result = loginService.login();
 		if (!result) {
-			System.exit(0);
+			return LifecycleRV.SHUTDOWN;
 		}
+		return LifecycleRV.CONTINUE;
 	}
 
 	@ProcessAdditions
